b0be244e86fc5330d49e923e8013421d9048cf2e
cohki0305 <cohki0305@gmail.com>
2018-07-16 12:10:47 +0900

    :+1: firebase と連携

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+import config from './firebase.config.js'
 
 Vue.config.productionTip = false
+
+firebase.initializeApp(config)
 
 new Vue({
   router,
