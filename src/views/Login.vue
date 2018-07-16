6ffebca54af829f1e2c04c6eb560519ee380a108
cohki0305 <cohki0305@gmail.com>
2018-07-16 14:49:23 +0900

    :+1: twitter login 機能を追加

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div
+    button(@click="login")
+      | Login
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  methods: {
+    async login() {
+      const provider = new firebase.auth.TwitterAuthProvider()
+      try {
+        await firebase.auth().signInWithPopup(provider)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
